865bf6155cecfd7a641514e4583f6e6c03b91792
jayjay <lima.jaine@escola.pr.gov.br>
2026-08-27 08:12:16 -0300

    Atualizar o script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,40 @@
 
+*script.js*
+//
+
+==
+
+// MODO ESCURO //
+
+const botaoTema = document.getElementById("botao Tema" );
+
+function alternarTema() {
+
+document.body.classList.toggle("escu ro");
+
+if
+
+uro")) { (document.body.classList.contains("esc
+
+botaoTema.textContent = " Modo claro";
+
+} else {
+
+botaoTema.textContent = " Modo escuro"; }
+
+}
+
+botaoTema.addEventListener("click", alternar Tema);
+
+//
+===================================
+// ALTERAR TEXTO //
+===================================
+
+botaoMensagem = document.getElementById("botaoMensa const gem");
+
+const ); mensagem = document.getElementById("mensagem"
+
+function alterarTexto() { mensagem.textContent = "O texto foi alterado pelo JavaScript!"; }
+
+botaoMensagem.addEventListener("clic k", alterar Texto);
